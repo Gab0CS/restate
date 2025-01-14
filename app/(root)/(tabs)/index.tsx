914422e7d17cc2bf386d1fc52aa418bfa3f561c3
Gab0CS <gabocanseco@gmail.com>
2025-01-13 23:35:26 -0600

    Project initialization

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className='font-bold text-2xl my-10'>Welcome to restate</Text>
+      <Link href="/sing-in">
+        Sign in
+      </Link>
+      <Link href="/explore">
+        Explore
+      </Link>
+      <Link href="/profile" className="">
+        Profile
+      </Link>
+      <Link href="/properties/1">
+        Sign in
+      </Link>
+    </View>
+  );
+}
