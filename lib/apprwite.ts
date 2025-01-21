5c72f7811df9cf09098a8bcd5ead798675437992
Gab0CS <gabocanseco@gmail.com>
2025-01-20 18:46:44 -0600

    Sing-in correction and appwrite.ts correction to

--- lib/apprwite.ts
+++ lib/apprwite.ts
@@ -1,5 +1,5 @@
 import {Account, Avatars, Client, OAuthProvider} from "react-native-appwrite";
-import * as linking from 'expo-linking';
+import * as Linking from 'expo-linking';
 import { openAuthSessionAsync } from "expo-web-browser";
 import { resolvePlugin } from "@babel/core";
 
@@ -21,59 +21,61 @@ export const account = new Account(client);
 
 export async function login() {
     try {
-        const redirectUri = linking.createURL('/');
-
-        const response = await account.createOAuth2Token(OAuthProvider.Google, redirectUri);
-
-        if(!response) throw new Error('Failed login');
-
-        const browserResult = await openAuthSessionAsync(
-            response.toString(),
-            redirectUri
-        )
-
-        if(browserResult.type != 'success') throw new Error("Failed login")
-
-        const url = new URL(browserResult.url);
-
-        const secret = url.searchParams.get('secret')?.toString();
-        const userId = url.searchParams.get('userId')?.toString();
-
-        if(!secret || !userId) throw new Error("Failed login");
-
-        const session = await account.createSession(userId, secret);
-
-        if (!session) throw new Error("Failed to create session")
-
-        return true;
-
+      const redirectUri = Linking.createURL("/");
+  
+      const response = await account.createOAuth2Token(
+        OAuthProvider.Google,
+        redirectUri
+      );
+      if (!response) throw new Error("Create OAuth2 token failed");
+  
+      const browserResult = await openAuthSessionAsync(
+        response.toString(),
+        redirectUri
+      );
+      if (browserResult.type !== "success")
+        throw new Error("Create OAuth2 token failed");
+  
+      const url = new URL(browserResult.url);
+      const secret = url.searchParams.get("secret")?.toString();
+      const userId = url.searchParams.get("userId")?.toString();
+      if (!secret || !userId) throw new Error("Create OAuth2 token failed");
+  
+      const session = await account.createSession(userId, secret);
+      if (!session) throw new Error("Failed to create session");
+  
+      return true;
     } catch (error) {
-        console.log(error)
-        return false;
+      console.error(error);
+      return false;
     }
-}
+  }
 
 export async function logout() {
     try {
-        await account.deleteSession("current")
+      const result = await account.deleteSession("current");
+      return result;
     } catch (error) {
-        console.log(error)
-        return false;
+      console.error(error);
+      return false;
     }
-}
+  }
 
-export async function getCurrentUser() {
+  export async function getCurrentUser() {
     try {
-        const response = await account.get();
-        if(response.$id){
-            const userAvatar = avatar.getInitials(response.name)
-            return {
-                ...response,
-                avatar: userAvatar.toString(),
-            }
-        }
+      const result = await account.get();
+      if (result.$id) {
+        const userAvatar = avatar.getInitials(result.name);
+  
+        return {
+          ...result,
+          avatar: userAvatar.toString(),
+        };
+      }
+  
+      return null;
     } catch (error) {
-        console.log(error)
-        return false;
+      console.log(error);
+      return null;
     }
-}
+  }
