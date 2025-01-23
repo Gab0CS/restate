164dc4c2351a8b8926fa75e630bb84e64559ce0a
Gab0CS <gabocanseco@gmail.com>
2025-01-22 18:44:01 -0600

    Flatlist added to index.tsx

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,20 +3,30 @@ import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
+import { useGlobalContext } from "@/lib/global-provider";
 import { Link } from "expo-router";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const { user } = useGlobalContext();
   return (
     <SafeAreaView className="bg-white h-full">
-      <View className="px-5">
+      <FlatList data={[1, 2, 3, 4] } 
+      renderItem={({item}) => <Card />} 
+      keyExtractor={(item) => item.toString()}
+      numColumns={2}
+      contentContainerClassName="pb-32"
+      columnWrapperClassName="flex gap-5 px-5"
+      showsVerticalScrollIndicator={false}
+      ListHeaderComponent={
+        <View className="px-5">
         <View className="flex flex-row items-center justify-between mt-5">
           <View className="flex flex-row">
-            <Image  source={images.avatar} className="size-12 rounded-full"/>
+            <Image  source={{ uri: user?.avatar }} className="size-12 rounded-full"/>
             <View className="flex flex-col items-start ml-2 justify-center">
               <Text className="text-xs font-rubik text-black-100">Good morning</Text>
-              <Text className="text-base font-rubik-medium text-black-300">Gabriel</Text>
+              <Text className="text-base font-rubik-medium text-black-300">{user?.name}</Text>
 
             </View>
           </View>
@@ -30,11 +40,16 @@ export default function Index() {
               <Text className="tex-base font-rubik-bold text-primary-300">See All</Text>
             </TouchableOpacity>
           </View>
-          <View className="flex flex-row gap-5 mt-5">
-            <FeaturedCard />
-            <FeaturedCard />
-            <FeaturedCard />
-          </View>
+          <FlatList 
+          data={[1,2,3]}
+          renderItem={({item}) => <FeaturedCard />}
+          keyExtractor={(item) => item.toString()}
+          horizontal
+          bounces={false}
+          showsHorizontalScrollIndicator={false}
+          contentContainerClassName="flex gap-5 mt-5"
+
+          />
         </View>
         <View className="flex flex-row items-center justify-between">
           <Text className="text-xl font-rubik-bold text-black-300 ">Our recomendation</Text>
@@ -43,11 +58,10 @@ export default function Index() {
           </TouchableOpacity>
         </View>
         <Filters />
-        <View className="flex flex-row gap-5 mt-5">
-          <Card />
-          <Card />
-        </View>
       </View>
+      }
+      />
+      
       
     </SafeAreaView>
   );
